7e8ad0d9ec5ba1d10fd184420dd119d38c83ea2c
Satche <info@thomas-robert.ch>
2024-11-23 11:10:16 +0100

    fix: path to config file

--- utils.py
+++ utils.py
@@ -4,6 +4,6 @@ import json
 
 def load_config():
     config_path = os.path.join(os.path.dirname(
-        os.path.abspath(__file__)), "../config.json")
+        os.path.abspath(__file__)), "config.json")
     with open(config_path, 'r') as config_file:
         return json.load(config_file)
