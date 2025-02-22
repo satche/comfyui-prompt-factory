f922cd1d65dc22eea34a9c1a1c384786f80185f1
Satche <info@thomas-robert.ch>
2025-02-22 19:53:17 +0100

    feat: handle subtags as variables

--- composer-test.py
+++ composer-test.py
@@ -13,5 +13,5 @@ def main(seed, prompt):
 
 if __name__ == "__main__":
     seed = 0
-    prompt = "foo, bar, {makeup-color}, {makeup}, {eyewear-style}"
+    prompt = "foo, bar, {makeup-color}, {makeup}, {eyewear-shape}"
     main(seed, prompt)
--- py/nodes/composer.py
+++ py/nodes/composer.py
@@ -1,7 +1,6 @@
-# from ..utils.config import load_nodes_config
 import numpy as np
 
-from ..utils.config import load_nodes_config
+from ..utils.config import load_nodes_config, RESERVED_KEYS
 from .node_factory import NodeFactory
 from .node_factory._tags import select_tags
 
@@ -12,7 +11,7 @@ class Composer:
     Inputs: string with {variables}
     Output: prompt composed with variables
     """
-    
+
     def __init__(self):
         self.data = load_nodes_config()
 
@@ -42,28 +41,37 @@ class Composer:
         Concatenate the tags and return the prompt
         """
         rng = np.random.default_rng(args["seed"])
-        variables = {}        
-        tags = {}
-
-        # Replace tags with corresponding variables
-        for key, value in self.data.items():
-            if "variables" in self.data[key]:
-                for v_key, v_value in self.data[key]["variables"].items():
-                    variables[v_key] = select_tags(rng, v_value)
-                tags = NodeFactory.apply_variables(tags, variables)
-            
-            if "tags" in self.data[key]:
-                for key, value in self.data[key]["tags"].items():
-                    tags[key] = select_tags(rng, value, p=1)
-                    
-                    """
-                    if isinstance(value, dict) and value not in RESERVED_KEYS:
-                        for sub_key, sub_value in value.items():
-                            tags[sub_key] = select_tags(rng, sub_value, p=1)
-                    """
+        variables = self._extract_variables(rng)
+        tags = self._extract_tags(rng)
 
         variables = {**tags, **variables}
         prompt = args["prompt"]
         prompt = NodeFactory.apply_variables(prompt, variables)
-        
+
         return (prompt,)
+
+    def _extract_variables(self, rng):
+        variables = {}
+        for key, value in self.data.items():
+            if "variables" in self.data[key]:
+                for v_key, v_value in self.data[key]["variables"].items():
+                    variables[v_key] = select_tags(rng, v_value)
+        return variables
+
+    def _extract_tags(self, rng):
+        tags = {}
+        for key, value in self.data.items():
+            if "tags" in self.data[key]:
+                for tag_key, tag_value in self.data[key]["tags"].items():
+                    tags[tag_key] = select_tags(rng, tag_value, p=1)
+                    if isinstance(tag_value, dict) and \
+                       tag_value not in RESERVED_KEYS:
+                        self._process_sub_tags(rng, tags, tag_value)
+        return tags
+
+    def _process_sub_tags(self, rng, tags, sub_tags):
+        for sub_key, sub_value in sub_tags.items():
+            if sub_key not in RESERVED_KEYS:
+                tags[sub_key] = select_tags(rng, sub_value, p=1)
+                if isinstance(sub_value, dict):
+                    self._process_sub_tags(rng, tags, sub_value)
