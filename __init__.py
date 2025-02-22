7f9eec7e6c3343bdc50c1f766344a3bb777fd2a3
Satche <info@thomas-robert.ch>
2025-02-22 19:52:31 +0100

    refactor: add register_node method for reusability

--- __init__.py
+++ __init__.py
@@ -1,10 +1,17 @@
 from .py.utils.config import load_nodes_config
-from .node_factory import NodeFactory
+from .py.nodes.node_factory import NodeFactory
 from .py.nodes.merge_strings import MergeStrings
+from .py.nodes.composer import Composer
 
 NODE_CLASS_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS = {}
 WEB_DIRECTORY = "./web"
+
+
+def register_node(node_id, node_name, node_class):
+    NODE_CLASS_MAPPINGS[node_id] = node_class
+    NODE_DISPLAY_NAME_MAPPINGS[node_id] = node_name
+
 
 # Create each node according to the config folder
 config = load_nodes_config()
@@ -13,12 +20,11 @@ for key, value in config.items():
     node_id = key
     node_name = value.get("name", key)
     ClassNode = NodeFactory.create_node(node_id, node_name)
-    NODE_CLASS_MAPPINGS[node_id] = ClassNode
-    NODE_DISPLAY_NAME_MAPPINGS[node_id] = node_name
+    register_node(node_id, node_name, ClassNode)
 
 # Add additional nodes
-NODE_CLASS_MAPPINGS["MergeStrings"] = MergeStrings
-NODE_DISPLAY_NAME_MAPPINGS["MergeStrings"] = "🪡 Merge Strings"
+register_node("MergeStrings", "🪡 Merge Strings", MergeStrings)
+register_node("Composer", "🖋️ Composer", Composer)
 
 __all__ = [
     "NODE_CLASS_MAPPINGS",
