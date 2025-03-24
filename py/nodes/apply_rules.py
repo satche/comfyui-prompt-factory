e0db742ab91161cd31ac65a6e9aac1caf4eba959
Satche <info@thomas-robert.ch>
2025-03-24 13:10:59 +0100

    feat: add apply_rules node

---
+++ py/nodes/apply_rules.py
@@ -0,0 +1,130 @@
+import fnmatch
+import dpath
+import numpy as np
+
+from py.utils.config import load_rules, load_nodes_config
+from py.nodes.node_factory._tags import select_tags
+
+
+class ApplyRules:
+    """
+    A ComfyUI node to apply rules to a string.
+    Input: string
+    Output: filtered string
+    """
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("string",)
+    FUNCTION = "apply_rules"
+    CATEGORY = "⚙️ Prompt Factory/🛠️ Utils"
+
+    def __init__(self):
+        self.config = load_nodes_config()
+        self.rules = load_rules()["rules"]
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "prompt": ("STRING", {"multiline": True}),
+                "seed": ("INT", {
+                    "default": 0,
+                    "min": 0,
+                    "max": 0xffffffffffffffff
+                }),
+            },
+            "optional": {}
+        }
+
+    def apply_rules(self, prompt, seed):
+        """
+        Apply the rules defined in config file.
+        Add and remove tags in the prompt
+        according to others tags in the same prompt
+        """
+        rng = np.random.default_rng(seed)
+        tags = prompt.split(", ")
+
+        for rule in self.rules:
+            triggers = rule["triggers"]
+            actions = rule["actions"]
+            p = rule.get("probability", 1)
+
+            if rng.random() > p:
+                continue
+
+            # If any triggering tag occurs in the prompt,
+            # run all the defined actions
+            for trigger in triggers:
+                if fnmatch.fnmatch(prompt, trigger):
+                    tags = run_actions(rng, actions, tags, self.config)
+                    break
+
+        return tags
+
+
+def run_actions(rng, actions, tags, config):
+    """
+    Run actions from the rule.
+    Add or remove tags with filtering tags
+    """
+
+    process_tags = tags.copy()
+
+    for action in actions:
+
+        type = action.get("type", "add")
+        value = action.get("value", [])
+        p = action.get("probability", 1)
+
+        if rng.random() > p:
+            continue
+
+        filtering_tags = process_action_value(value, config)
+
+        match type:
+
+            case "add":
+                value = select_tags(rng, filtering_tags)
+                process_tags.append(value)
+
+            case "remove":
+
+                # filter: remove tags that match any "remove" value
+                process_tags = [tag for tag in process_tags
+                                if not any(
+                                    fnmatch.fnmatch(tag, f"*{v}*")
+                                    for v in filtering_tags
+                                )]
+
+    return process_tags
+
+
+def process_action_value(value, config):
+    """
+    Process the action's value.
+    Return selected tags randomly or according to a given path
+    """
+
+    tags = []
+
+    if isinstance(value, str):
+        value = [value]
+
+    # If the value is a path,
+    # take all the tags from this path
+    for v in value:
+        if is_path(v):
+            for k, i in dpath.search(config, v,
+                                     yielded=True, dirs=False):
+                if isinstance(i, str):
+                    tags.append(i)
+        else:
+            tags.append(v)
+
+    return tags
+
+
+def is_path(string):
+    "Check if the value is a glob path"
+    is_path = isinstance(string, str) and ("/" in string or "*" in string)
+    return is_path
